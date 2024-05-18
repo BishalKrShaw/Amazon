2cb9ebca7f0280623d7124406f1fefd65893ab7d
BishalKrShaw <bishalshaw.mfis@gmail.com>
2024-05-18 17:47:10 +0530

    Moved artToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,22 @@
 export const cart = []; /* export is used to export this variable to use it. This variable can be accessed where import is written like this => import {cart} from '../data/cart.js' -> basically it;s path */
+
+export function addToCart(productId) {
+    let matchingItem;
+
+        cart.forEach((cartItem) => {
+            if(productId === cartItem.productId) {
+                matchingItem = cartItem;
+            }
+        });
+
+        if(matchingItem) {
+            matchingItem.quantity += 1;
+        } else{
+            cart.push(
+                {
+                    productId: productId,
+                    quantity: 1,
+                },
+            );
+        }
+}
